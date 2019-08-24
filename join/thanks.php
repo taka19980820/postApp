5eaf8626e167cac19b69d9b2df0fb6cbcf494f0e
taka19980820 <49720214+taka19980820@users.noreply.github.com>
2019-08-24 21:35:05 +0900

    Create thanks.php

---
+++ join/thanks.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once(__DIR__ . '/../config.php');
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>postApp</title>
+    <link rel="stylesheet" href="/../styles.css">
+  </head>
+  <body>
+    <header>
+      <div id="container">
+      <h1>postApp</h1>
+    </div>
+    </header>
+    <div id="container">
+      <p>ユーザー登録が完了しました。</p>
+      <a href="../login.php">ログインする</a>
+    </div>
+  </body>
+</html>
